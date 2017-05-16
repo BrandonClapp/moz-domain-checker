4ff15d95f93054a499ec5eee3a6d2ac448ca3c17
Brandon Clapp <brandonclapp@outlook.com>
2017-05-15 21:05:42 -0500

    init

--- checker.py
+++ checker.py
@@ -1,9 +1,10 @@
+import math
 import requests
 import json
 from domain_importer import content as domains
 import result_exporter
-
-#print(domains)
+from datetime import datetime
+import time
 
 cookies = {
     'mozauth': 'gHiPdyGUi7wAqNLLj0BVAeLmYlLvWFe14k6vP2ee7UovZTiZlcrTDxRdL75WtSij'
@@ -16,11 +17,28 @@ headers = {
 for domain in domains:
 
     payload = [domain]
-    resp = requests.post("https://mozbar.moz.com/bartender/url-metrics", data=json.dumps(payload), cookies=cookies, headers=headers)
+    # print('----- Making Request -----')
+    try:
+        resp = requests.post("https://mozbar.moz.com/bartender/url-metrics", data=json.dumps(payload), cookies=cookies, headers=headers, timeout=10)
+    except requests.exceptions.Timeout:
+        print('Domain %s timed out. Adding to skip file.' %(domain))
+        continue
+    # print('----- Request Complete -----')
 
+    # print('----- Deserializing Response -----')
     response_body = json.loads(resp.text)
     response_body = response_body[0]
     # print(resp.headers)
+    # print('----- Deserializing Response Complete -----')
+    remaining_count = resp.headers.get('X-RateLimit-Remaining')
+    reset_time_unix = resp.headers.get('X-RateLimit-Reset')
+
+    reset_time = datetime.fromtimestamp(int(reset_time_unix))
+    print('Remaining: %s' %(remaining_count))
+    print('Reset time: %s' %(reset_time))
+
+    reset_seconds = math.floor((reset_time - datetime.now()).seconds)
+    print('Seconds until reset: ', reset_seconds)
 
     pa = round(response_body.get('pda'), 2)
     da = round(response_body.get('upa'), 2)
@@ -31,6 +49,10 @@ for domain in domains:
         'pa': pa
     }
 
-     # print('DA = %s, PA = %s' %(da, pa))
     print(rpt)
     result_exporter.write(json.dumps(rpt))
+
+    if int(remaining_count) == 0:
+        print('Sleeping for...')
+        print(reset_seconds)
+        time.sleep(reset_seconds + 5)
--- result_exporter.py
+++ result_exporter.py
@@ -1,3 +1,8 @@
 def write(item):
     with open('results.txt', 'a') as result_file: # a for append
         result_file.write(item + ',\n')
+
+
+def skip(domain):
+    with open('skipped.txt', 'a') as skip_file:
+        skip_file.write(domain + '\n')
