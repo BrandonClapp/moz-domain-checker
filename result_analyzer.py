14e866699d683e101db7eec8160525b312d27f6d
Brandon Clapp <clapp09@gmail.com>
2017-05-15 22:31:13 -0500

    Ready for running

--- result_analyzer.py
+++ result_analyzer.py
@@ -1,1 +1,14 @@
 import json_lines
+
+da_threshold = 20
+pa_threshold = 10
+
+consider_list = []
+
+with open('sample-analyze.jl', 'rb') as file:
+    for line in json_lines.reader(file):
+        if line['da'] >= da_threshold and line['pa'] >= pa_threshold:
+            consider_list.append(line)
+
+for item in consider_list:
+    print(item['domain'], '\t', item['da'], '\t', item['pa'])
--- result_exporter.py
+++ result_exporter.py
@@ -1,9 +1,4 @@
 def write(item):
     # write json lines
-    with open('results.txt', 'a') as result_file: # a for append
+    with open('results.jl', 'a') as result_file: # a for append
         result_file.write(item + '\n')
-
-
-def skip(domain):
-    with open('skipped.txt', 'a') as skip_file:
-        skip_file.write(domain + '\n')
